0e1a3d1283634ba02f00fa34f0fba7aadf1413e7
Taz <47963508+Web-Dev-Taz@users.noreply.github.com>
2022-07-13 19:37:57 -0600

    bare bones index

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import Head from "next/head"
-
 export default function Home() {
    return (
-      <div>
-         <Head>
-            <title></title>
-            <meta name="description" content="" />
-            <link rel="icon" href="/favicon.ico" />
-         </Head>
-
-         <h1>Welcome!</h1>
+      <div className="mt-10">
+         <div className="text-center p-4 m-4">
+            <h2 className="mb-10 text-4xl font-bold">Find a job!</h2>
+         </div>
       </div>
    )
 }
